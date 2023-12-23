cd50767bb22a347caea90caf8f929147011796e1
TugberkAlp <tugberkharman@gmail.com>
2023-12-24 00:30:56 +0300

    24-12-00.30

--- scripts/post.js
+++ scripts/post.js
@@ -1,18 +1,18 @@
+import { userInformation, writeTwits } from "./userInformation.js";
+
 const postButton = document.getElementById("js-post-button");
 const postXButton = document.getElementById("js-post-x-button");
 const mainArea = document.getElementById('js-main');
 const postArea = document.getElementById('js-post-area');
+const postInput = document.getElementById('js-post-input');
+const postAreaButton = document.getElementById('js-post-area-button');
 
 function checkInput() {
-  const textAreaValue = document.getElementById('js-post-input').value;
-  const postAreaButton = document.getElementById('js-post-area-button');
-
-  if (textAreaValue.trim() === '') {
-    postAreaButton.disabled = true;
-  } else {
-    postAreaButton.disabled = false;
-  }
+  const textAreaValue = postInput.value.trim();
+  postAreaButton.disabled = textAreaValue === '';
 }
+
+postInput.addEventListener("input", checkInput);
 
 postButton.addEventListener("click", function (event) {
   event.stopPropagation();
@@ -31,6 +31,14 @@ postButton.addEventListener("click", function (event) {
 postXButton.addEventListener("click", function () {
   postArea.style.display = 'none';
   closePostArea();
+});
+
+postAreaButton.addEventListener("click", function () {
+  const textAreaValue = postInput.value;
+  userInformation.twits.unshift(textAreaValue);
+  writeTwits();
+  closePostArea();
+  postInput.value = "";
 });
 
 function closePostArea() {
--- scripts/userInformation.js
+++ scripts/userInformation.js
@@ -1,6 +1,8 @@
-let userInformation = {
+
+export const userInformation = {
   username: "Alpturk HARMAN",
-  follows: 10,
+  usernameTag: "@alpturk_harman",
+  following: 10,
   followers: 12,
   twits: ["Bu bir twit", "Yeni bir twit"]
 };
@@ -9,6 +11,18 @@ const setupButton = document.getElementById('js-setup');
 const nameSetup = document.getElementById("js-nameInput");
 const jsName = document.getElementById("js-name");
 const jsNames = document.querySelectorAll(".js-name");
+const tweetsList = document.querySelector(".tweets-list");
+
+writeUserInfos(userInformation);
+writeTwits(userInformation);
+
+function writeUserInfos() {
+  document.getElementById('js-name').innerHTML = userInformation.username;
+  document.getElementById('js-nameTag').innerHTML = userInformation.usernameTag;
+  document.getElementById('following').innerHTML = userInformation.following;
+  document.getElementById('follower').innerHTML = userInformation.followers;
+}
+
 
 setupButton.addEventListener("click", function () {
   if (setupButton.innerHTML === "Confirm") {
@@ -18,6 +32,8 @@ setupButton.addEventListener("click", function () {
       nameSetup.style.display = "none";
       userInformation.username = nameSetup.value;
       changeNames();
+      writeTwits(userInformation);
+      nameSetup.value = "";
     } else {
       alert("Please enter a username.");
       return;
@@ -35,6 +51,55 @@ function changeNames() {
   })
 };
 
-function writeTwits() {
-  
-}
+export function writeTwits() {
+  tweetsList.innerHTML = "";
+  userInformation.twits.forEach(function (tweet) {
+    tweetsList.innerHTML += `
+  <div class="tweets">
+                  <div class="tweets-img-div">
+                    <img class="tweetsImg" src="https://xsgames.co/randomusers/avatar.php?g=male">
+                  </div>
+                  <div class="tweets-right">
+                    <div class="tweetsNameDiv">
+                      <div class="tweetsNameDiv2">
+                        <div class="tweetsName js-name">${userInformation.username}</div>
+                        <div class="tweetsNameTag js-nameTag">@alpturk_harman</div>
+                      </div>
+                      <div class="dots-img">
+                        <img class="little-3-dot-img" src="images/3-dot.png">
+                      </div>
+                    </div>
+                    <div class="tweets-contents">
+                      ${tweet}
+                    </div>
+                    <div class="tweets-icons">
+                      <div class="tweets-reply-div">
+                        <img class="tweets-reply" src="images/reply.png">
+                        <div class="tweets-reply-number"></div>
+                      </div>
+                      <div class="tweets-retweet-div">
+                        <img class="tweets-retweet" src="images/retweet.png">
+                        <div class="tweets-retweet-number"></div>
+                      </div>
+                      <div class="tweets-like-div">
+                        <img class="tweets-like" src="images/like.png">
+                        <div class="tweets-like-number"></div>
+                      </div>
+                      <div class="tweets-view-div">
+                        <img class="tweets-view" src="images/views.png">
+                        <div class="tweets-view-number"></div>
+                      </div>
+                      <div class="tweets-little-div">
+                        <div class="tweets-bookmark-div">
+                          <img class="tweets-bookmark" src="images/bookmark.png">
+                        </div>
+                        <div class="tweets-share-div">
+                          <img class="tweets-share" src="images/share.png">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+  `;
+  })
+};
